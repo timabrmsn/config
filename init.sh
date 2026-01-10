d308d7d0a5e8d390d6d9a29d054cf53ea449dc19
Tim <tim@abramson.dev>
2026-01-10 10:26:47 -0600

    change shell in init

--- init.sh
+++ init.sh
@@ -9,6 +9,7 @@ sudo pacman -Sy zsh --noconfirm 2>/dev/null || \
 sudo zypper in -y zsh 2>/dev/null || \
 sudo apk add zsh 2>/dev/null
 
+chsh -s $(which zsh)
 curl https://mise.run/zsh | sh
 curl https://raw.githubusercontent.com/timabrmsn/config/refs/heads/main/mise.toml -o ~/.config/mise.toml
 mise run sync
