f3956c3dda192e03ee308c1240271188999c4f16
Tim <tim@abramson.dev>
2026-01-10 09:43:38 -0600

    add init.sh

---
+++ init.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Universal linux zsh installation
+/usr/bin/env zsh --version || \
+sudo apt install zsh -y 2>/dev/null || \
+sudo dnf install zsh -y 2>/dev/null || \
+sudo yum install zsh -y 2>/dev/null || \
+sudo pacman -Sy zsh --noconfirm 2>/dev/null || \
+sudo zypper in -y zsh 2>/dev/null || \
+sudo apk add zsh 2>/dev/null
+
+curl https://mise.run/zsh | sh
+curl https://raw.githubusercontent.com/timabrmsn/config/refs/heads/main/mise.toml -o ~/.config/mise.toml
+mise run sync
